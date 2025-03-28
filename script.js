d1f6ed8a6544f8c6575b622e652473406b1797f1
Giacomo Salsano <giacomosalsano@hotmail.com>
2025-03-28 18:52:58 -0300

    fix: fix function exportation

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-import { config } from './config.js';
-
-const BIN_ID = config.JSONBIN_BIN_ID;
-const API_KEY = config.JSONBIN_API_KEY;
-
-
 // Função de toggle
 export function toggleMode() {
     const html = document.documentElement
@@ -30,9 +24,9 @@ function updateInterface(likes) {
 // Função para carregar likes
 async function loadLikes() {
     try {
-        const response = await fetch(`https://api.jsonbin.io/v3/b/${BIN_ID}/latest`, {
+        const response = await fetch(`https://api.jsonbin.io/v3/b/67e6ee3d8a456b79667e8fe7/latest`, {
             headers: {
-                'X-Master-Key': API_KEY
+                'X-Master-Key': '$2a$10$23Oft0rvtncbSQEuk4LWtOADdg0TXJucic9fQkHgTwcM0TDjGppsC'
             }
         });
         const data = await response.json();
@@ -54,20 +48,20 @@ async function loadLikes() {
 async function incrementLikes() {
     try {
         // Busca o número atual
-        const response = await fetch(`https://api.jsonbin.io/v3/b/${BIN_ID}/latest`, {
+        const response = await fetch(`https://api.jsonbin.io/v3/b/67e6ee3d8a456b79667e8fe7/latest`, {
             headers: {
-                'X-Master-Key': API_KEY
+                'X-Master-Key': '$2a$10$23Oft0rvtncbSQEuk4LWtOADdg0TXJucic9fQkHgTwcM0TDjGppsC'
             }
         });
         const data = await response.json();
         const newLikes = (data.record.likes || 0) + 1;
         
         // Salva o novo número
-        await fetch(`https://api.jsonbin.io/v3/b/${BIN_ID}`, {
+        await fetch(`https://api.jsonbin.io/v3/b/67e6ee3d8a456b79667e8fe7`, {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
-                'X-Master-Key': API_KEY
+                'X-Master-Key': '$2a$10$23Oft0rvtncbSQEuk4LWtOADdg0TXJucic9fQkHgTwcM0TDjGppsC'
             },
             body: JSON.stringify({ likes: newLikes })
         });
